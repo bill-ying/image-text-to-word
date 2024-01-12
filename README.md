584a3db7382c947323392fa3c65b598bc0ade9e7
bill <bill@billthink>
2024-01-12 15:33:51 -0500

    Initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+OCR image files from input folder and write the text to a Word document.
+
+Input parameters:
+- input-folder (the folder containing all the image files)
+- output-file (the output file with .docx extension)
+
+The program depends on packages:
+- pytesseract
+- python-docx
+
+The following program must be installed on the computer running this program:
+- pytesseract-ocr
+
+Example:
+- python image-text-to-word /home/user/images /home/user/output.docx
---
+++ image-text-to-word.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import argparse
+
+import pytesseract
+from PIL import Image
+from docx import Document
+
+
+def main(input_folder, output_file):
+    document = Document()
+
+    for filename in sorted(os.listdir(input_folder)):
+        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
+            image = Image.open(os.path.join(input_folder, filename))
+            text = pytesseract.image_to_string(image)
+            document.add_paragraph(text)
+
+    document.save(output_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description
+                                     ='OCR images (png or jpg) of the input folder, and write the text to a '
+                                      'Word document.')
+    parser.add_argument('input_folder', help='the input directory for images')
+    parser.add_argument('output_file', help='the Word output file with docx extension.')
+    args = parser.parse_args()
+    sys.exit(main(args.input_folder, args.output_file))
